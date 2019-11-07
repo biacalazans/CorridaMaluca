48452fbafe89f9b2c39fc1206f809ed38147b028
biacalazans <50582382+biacalazans@users.noreply.github.com>
2019-11-07 07:41:02 -0300

    ghffjy

---
+++ Cow.py
@@ -0,0 +1,7 @@
+class Cow:
+    def __init__(self, x, y, img, vel):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.vel = vel
+        # self.hit = hit
---
+++ Dino.py
@@ -0,0 +1,7 @@
+class Dino:
+    def __init__(self, x, y, img, vel):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.vel = vel
+        # self.hit = hit
